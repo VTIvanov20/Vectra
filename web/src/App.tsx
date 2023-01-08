a15f3b9abdeb6a8911b5cd6f7369091f486981d4
VTIvanov20 <vtivanov20@codingburgas.bg>
2023-01-08 19:23:33 +0200

    Add mafs structure basis  * amongst lib styling

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,32 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import {
+  Mafs,
+  Text, 
+  useMovablePoint, 
+  CartesianCoordinates 
+} from "mafs"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const point = useMovablePoint([0, 0]);
+  let height = window.innerHeight;
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Mafs height={height}>
+        <CartesianCoordinates subdivisions={2} />
+        
+        <Text x={-4.3} y={2.5} attach="n" color="#5d8ca3">
+          hello world
+        </Text>
+      </Mafs>
     </div>
   )
 }
