a342b87e2451e7168e6a4ee13c0a8e1990bdb773
VTIvanov20 <vtivanov20@codingburgas.bg>
2023-01-10 19:13:18 +0200

    Add base of interactive triangle  * Implement basic coordinate math  * Chore: switch array -> map

--- web/src/pages/Index.tsx
+++ web/src/pages/Index.tsx
@@ -3,32 +3,32 @@ import {
   CartesianCoordinates,
   Mafs, Text, Line, Circle,
   Polygon, Vector2,
-  useMovablePoint, vec
+  useMovablePoint, vec, Plot
 } from 'mafs';
 import * as React from 'react';
 
-type Points = Vector2[];
+type GraphPoints = Vector2[];
 
 const Index: React.FC = (props) => {
   let [height, setHeight] = useState(window.innerHeight);
-
+  
   useEffect(() => {
     function handleResize(this: Window, ev: UIEvent) {
       setHeight(window.innerHeight);
     }
-
+    
     window.addEventListener('resize', handleResize);
-
+    
     return () => {
       window.removeEventListener('resize', handleResize);
     }
   })
-
+  
   
   const lineStart = useMovablePoint([3.35, -0.2]);
   const lineEnd = useMovablePoint([1.9, -0.2]);
   
-  const col1: Points = [[-6, -7], [-5.5, -7], [-5.5, -3], [-6, -3]]
+  const col1: GraphPoints = [[-6, -7], [-5.5, -7], [-5.5, -3], [-6, -3]]
   
   // bottom left, bottom right, top right, top left 
   const blValue = col1[0][0], brValue = col1[0][1], 
@@ -36,26 +36,26 @@ const Index: React.FC = (props) => {
   
   {/* 
   to simplify total positioning, the code has been reformatted
-  const col2: Points = [[-5.5, -7], [-5, -7], [  -5,   -2], [  -5.5, -2]]
+  const col2: GraphPoints = [[-5.5, -7], [-5, -7], [  -5,   -2], [  -5.5, -2]]
   */}
 
-  const col2: Points = [
+  const col2: GraphPoints = [
     [(blValue + 0.5), brValue],
     [(trValue + 0.5), brValue], 
     [(trValue + 0.5), (tlValue + 0.5)], 
     [(blValue + 0.5), (tlValue + 0.5)]
   ]
 
-  // const col3: Points = [[-4.99, -7], [-4.5, -7], [-4.5, -1.5], [-4.99, -1.5]]
-  const col3: Points = [
+  // const col3: GraphPoints = [[-4.99, -7], [-4.5, -7], [-4.5, -1.5], [-4.99, -1.5]]
+  const col3: GraphPoints = [
     [(blValue + 1.01), brValue],
     [(trValue + 1), brValue], 
     [(trValue + 1), (tlValue + 1.5)], 
     [(blValue + 1.01), (tlValue + 1.5)]
   ]
 
-  // const col4: Points = [[-4.99, -7], [-4.5, -7], [-4.5, -1.5], [-4.99, -1.5]]
-  const col4: Points = [
+  // const col4: GraphPoints = [[-4.99, -7], [-4.5, -7], [-4.5, -1.5], [-4.99, -1.5]]
+  const col4: GraphPoints = [
     [(blValue + 1.51), brValue],
     [(trValue + 1.5), brValue], 
     [(trValue + 1.5), (tlValue + 1)], 
@@ -63,21 +63,35 @@ const Index: React.FC = (props) => {
   ]
 
   // no comment :(
-  const col5: Points = [
+  const col5: GraphPoints = [
     [(blValue + 2), brValue],
     [(trValue + 2), brValue], 
     [(trValue + 2), (tlValue + 0.5)], 
     [(blValue + 2), (tlValue + 0.5)]
   ]
   
-  const circleDrag = useMovablePoint([
-    Math.sqrt(49) / 7,
-    Math.sqrt(49) / 7,
-    //7, 3.5
-  ]);
+  // borken, will fix later on
+  // const circleDrag = useMovablePoint([
+  //   Math.sqrt(49) / 7,
+  //   Math.sqrt(49) / 7,
+  //   7, 3.5
+  // ]);
 
-  const radius = vec.mag(circleDrag.point)
-  
+  // const radius = vec.mag(circleDrag.point)
+
+
+  //TODO: simplify the following lines, by using a map
+  const triArr: Array<number> = [6, -0.5]
+
+  // used as base
+  const triA = useMovablePoint([triArr[0], triArr[1]])
+
+  // points have to be [2, -2]
+  const triB = useMovablePoint([triArr[0] - 4, triArr[1] - 1.5])
+
+  // points have to be [4, -3]
+  const triC = useMovablePoint([triArr[0] - 2, triArr[1] - 2.5])
+
 
   return <Mafs pan={false} height={height}>
     <CartesianCoordinates 
@@ -104,29 +118,41 @@ const Index: React.FC = (props) => {
       point2={lineEnd.point}
     />
 
-    <Circle center={[7, 3.5]} radius={radius}/>
+    {/* <Circle center={[7, 3.5]} radius={3} color="#FF2121"/> */}
 
+    {/* Begining of graphs */}
     <Polygon 
       points={col1}
       color="#D73DFE"
-    />
+      />
     <Polygon 
       points={col2}
       color="#D73DFE"
-    />
+      />
     <Polygon 
       points={col3}
       color="#86FE3D"
-    />
+      />
     <Polygon 
       points={col4}
       color="#86FE3D"
-    />
+      />
     <Polygon 
       points={col5}
       color="#86FE3D"
-    />
-    {circleDrag.element}
+      />
+    {/* End of graphs */}
+
+    {/* Begining of triangle polygon */}
+    <Polygon 
+      points={[triA.point, triB.point, triC.point]}
+      color="#2F94FF"
+      />
+    {/* End of triangle polygon */}
+
+    {triA.element}
+    {triB.element}
+    {triC.element}
   </Mafs>
 }
 
